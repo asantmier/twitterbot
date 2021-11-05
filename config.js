f27de82c700fc09f39481c2ddbc6d25a850441a8
Rosebud24246 <67978054+Rosebud24246@users.noreply.github.com>
2021-11-04 20:46:56 -0400

    Update config.js
    Updated all keys

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         'eFX9Jl8F5H4qQCpLeYz5sj6Nb',
+  consumer_secret:      'N5hmggQ1mopQ5pO5z3YDgXcPG4LpCoYE8ZCqMUV8Qy0DJqa0Hx',
+  access_token:         '1452330567844999169-12mBU5kpJhKf6QVLzJTVZ9qJZxXZ8k',
+  access_token_secret:  'bKlT8TyGjSTsUzpCUxY56Hf0toTwEnyjFnO4iIMIc8Ama'
 }
