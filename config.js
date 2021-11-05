4a59331a1a5eb37cde6991a382a68b0095c31cc5
Rosebud24246 <67978054+Rosebud24246@users.noreply.github.com>
2021-11-04 21:58:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 module.exports = {
   consumer_key:         'eFX9Jl8F5H4qQCpLeYz5sj6Nb',
   consumer_secret:      'N5hmggQ1mopQ5pO5z3YDgXcPG4LpCoYE8ZCqMUV8Qy0DJqa0Hx',
-  access_token:         '1452330567844999169-12mBU5kpJhKf6QVLzJTVZ9qJZxXZ8k',
-  access_token_secret:  'bKlT8TyGjSTsUzpCUxY56Hf0toTwEnyjFnO4iIMIc8Ama'
+  access_token:         '1452330567844999169-164rMnonJ9WeYrTU0JoFsCOJY15h6f',
+  access_token_secret:  'OetcEW798qxLpJkrpKxxAucJkMNrokEPnODyfyUsc0x0u'
 }
